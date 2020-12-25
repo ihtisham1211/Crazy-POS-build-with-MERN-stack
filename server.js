43072ef31ad5bc8b33b8925deeddc91afac5c555
ihtisham arif <im.ihtisham.arif@gmail.com>
2020-12-25 21:05:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectToDb = require("./Database/db");
 var cors = require("cors");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 //init express
@@ -22,6 +23,14 @@ app.get("/", (req, res) => {
 app.use("/api/user", require("./routers/user"));
 app.use("/api/auth", require("./routers/auth"));
 app.use("/api/product", require("./routers/product"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
