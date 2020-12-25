1a149773200a9fa6b30fa34da402a3241e5dcc8d
ihtisham arif <im.ihtisham.arif@gmail.com>
2020-12-25 21:33:43 +0500

    Update server.js

--- server.js
+++ server.js
@@ -24,14 +24,8 @@ app.use("/api/user", require("./routers/user"));
 app.use("/api/auth", require("./routers/auth"));
 app.use("/api/product", require("./routers/product"));
 
-// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    app.use(express.static('client/build'));
 }
 
 app.listen(PORT, () => {
